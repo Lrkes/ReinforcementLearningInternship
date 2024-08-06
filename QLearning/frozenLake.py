5148d4e7c9ca3a29b4e14695f721a11de670831f
361626 <lukas.bonkowski@thluebeck.de>
2024-08-06 10:17:11 +0900

    rem reward over time plot

--- QLearning/frozenLake.py
+++ QLearning/frozenLake.py
@@ -9,24 +9,25 @@ def main():
 
     state_size = env.observation_space.n
     action_size = env.action_space.n
+    
     qtable = np.zeros((state_size, action_size))
-    state_visits = np.zeros(state_size)
 
+    # Hyperparameter
     learning_rate = 0.825
     discount_rate = 0.9
     epsilon = 1.0
     decay_rate = 0.005
 
+    # Episodes
     num_episodes = 5000
     max_steps = 100
     
-    heatmap_interval = 250
-    num_heatmaps = num_episodes // heatmap_interval
-
+    # For plots
+    state_visits = np.zeros(state_size)
+    plot_interval = 250
+    num_heatmaps = num_episodes // plot_interval
     all_state_visits = np.zeros((num_heatmaps, state_size))
 
-    action_labels = ['←', '↓', '→', '↑']
-    rewards_per_episode = []
     win_count_per_interval = []
     current_win_count = 0
 
@@ -70,29 +71,20 @@ def main():
                 break
             
 
-        rewards_per_episode.append(total_reward)
         
         if total_reward > 0:
             current_win_count += 1
         
         epsilon = np.exp(-decay_rate * episode)
 
-        if not (episode + 1) % heatmap_interval:
-            all_state_visits[episode // heatmap_interval] = state_visits
-            win_count_per_interval.append(current_win_count * 100 / heatmap_interval)
+        if not (episode + 1) % plot_interval:
+            all_state_visits[episode // plot_interval] = state_visits
+            win_count_per_interval.append(current_win_count * 100 / plot_interval)
             current_win_count, state_visits = 0, np.zeros(state_size)
 
     env.close()
 
-    print(f"Total rewards: {sum(rewards_per_episode)}")
-
-    best_actions = [np.argmax(qtable[state, :]) for state in range(state_size)]
-    actions = [
-        'x' if state in [5, 7, 11, 12] else ('o' if state == 15 else action_labels[best_actions[state]])
-        for state in range(state_size)
-    ]
-
-    best_action_grid = np.array(actions).reshape((4, 4))
+    # Plotting results
 
     # 1. Heatmaps with State Visits per 250 Episodes
     fig, axes = plt.subplots(nrows=4, ncols=5, figsize=(15, 15))
@@ -103,7 +95,7 @@ def main():
         sns.heatmap(all_state_visits[i].reshape(4, 4), annot=True, cmap="YlGnBu", fmt=".0f", ax=ax)
         for (x, y), color in [((0, 3), "red"), ((1, 1), "red"), ((3, 1), "red"), ((3, 2), "red"), ((3, 3), "green")]:
             ax.add_patch(plt.Rectangle((x, y), 1, 1, fill=False, edgecolor=color, lw=2))
-        ax.set_title(f"Episodes {i * heatmap_interval}-{i * heatmap_interval + heatmap_interval - 1}")
+        ax.set_title(f"Episodes {i * plot_interval}-{i * plot_interval + plot_interval - 1}")
         ax.set_xlabel("Column")
         ax.set_ylabel("Row")
     plt.tight_layout()
@@ -111,6 +103,14 @@ def main():
     plt.show()
 
     # 2. Best actions Map
+    action_labels = ['←', '↓', '→', '↑']
+    best_actions = [np.argmax(qtable[state, :]) for state in range(state_size)]
+    actions = [
+        'x' if state in [5, 7, 11, 12] else ('o' if state == 15 else action_labels[best_actions[state]])
+        for state in range(state_size)
+    ]
+
+    best_action_grid = np.array(actions).reshape((4, 4))
     fig, ax = plt.subplots()
     ax.axis('off')
     ax.axis('tight')
@@ -119,38 +119,18 @@ def main():
     table.set_fontsize(12)
     table.scale(1, 2)
     plt.savefig('Visualization/frozenLake/best_actions2.png')
-
     plt.show()
 
-    # 3. Shows Reward over time
-    episodes_per_split = 1000
-    splits = num_episodes // episodes_per_split
-
-    fig, axes = plt.subplots(nrows=splits, ncols=1, figsize=(12, 6 * splits))
-    for i in range(splits): 
-        start = i * episodes_per_split
-        end = (i + 1) * episodes_per_split
-        ax = axes[i]
-        ax.plot(range(start, end), rewards_per_episode[start:end])
-        ax.set_xlabel("Episode")
-        ax.set_ylabel("Total Reward")
-        ax.set_title(f"Total Reward per Episode (Episodes {start + 1} to {end})")
-    plt.tight_layout()
-    plt.savefig('Visualization/frozenLake/reward2.png')
-    plt.show()
-
-    # 4. Plot the win percentage per 250 episodes
+    # 3. Plot the win percentage per 250 episodes
     plt.figure(figsize=(12, 6))
-    plt.plot(range(heatmap_interval, num_episodes + 1, heatmap_interval), win_count_per_interval, marker='o')
+    plt.plot(range(plot_interval, num_episodes + 1, plot_interval), win_count_per_interval, marker='o')
     plt.xlabel("Episode")
     plt.ylabel("Win Percentage")
     plt.title("Win Percentage per 250 Episodes")
     plt.savefig('Visualization/frozenLake/win_percentage2.png')
-
     plt.show()
 
-
-     # 5. Q-Value over time
+     # 4. Q-Value over time
     fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(15, 12))
     
     for state in range(state_size):
@@ -162,7 +142,6 @@ def main():
         axes[row, col].set_ylabel('Q-value', fontsize=10)
         axes[row, col].set_title(f'State {state}', fontsize=12)
         axes[row, col].legend(fontsize=8)
-
 
     plt.suptitle('Q-values for each Action in each State Over 250 Episodes', y=1.02, fontsize=14)
     plt.tight_layout()
